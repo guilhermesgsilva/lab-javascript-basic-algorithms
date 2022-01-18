b8036907ba9de18b4d9c22f8b9ca695b49889150
Guilherme Silva <silva.gsg@gmail.com>
2022-01-18 12:00:12 +0000

    lab done

--- js/index.js
+++ js/index.js
@@ -1,81 +1,112 @@
 // Iteration 1: Names and Input
 
 const hacker1 = "Guilherme";
-console.log(`The drivers name is ${hacker1}.`)
+console.log(`"The driver's name is ${hacker1}"`);
 
-const hacker2 = "Guilherma";
-console.log(`The navigator's name is ${hacker2}.`)
+const hacker2 = "Karina";
+console.log(`"The navigator's name is ${hacker2}"`);
 
 
 // Iteration 2: Conditionals
 
- if (hacker1.length > hacker2.length) {
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
- } else if (hacker1.length < hacker2.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
- } else if (hacker1.length === hacker2.length) {
-    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
- } else {
-    console("try again");
- };
+if (hacker1.length > hacker2.length) {
+   console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+   console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else if (hacker1.length === hacker2.length) {
+   console.log(`Wow, you both have equally long names, XX characters!`);
+} else {
+   console("try again");
+};
+
 
 // Iteration 3: Loops
 
+//3.1
+let hacker1Capitals = hacker1.toUpperCase();
 
-let hacker1Capitals = hacker1.toUpperCase()
-
-console.log(hacker1Capitals);
-
-let hacker1SeparatedCapitals = "";
+let hacker1CapitalsSpaced = "";
 
 for (let i = 0; i < hacker1Capitals.length; i++) {
-    hacker1SeparatedCapitals = hacker1SeparatedCapitals + hacker1Capitals[i] + " ";
+   if (i < hacker1Capitals.length - 1) {
+      hacker1CapitalsSpaced += hacker1Capitals[i] + " "
+   } else {
+      hacker1CapitalsSpaced += hacker1Capitals[i]
+   };
 };
 
-console.log(hacker1SeparatedCapitals);
+console.log(hacker1CapitalsSpaced);
 
+//3.2
+let hacker2Reverse = "";
 
-let hacker2Backwards = "";
-for (let i = hacker2.length - 1; i>=0; i--) {
-    hacker2Backwards = hacker2Backwards + hacker2[i];
+for (let i = hacker2.length - 1; i >= 0; i--) {
+   hacker2Reverse += hacker2[i]
 }
 
-console.log(hacker2Backwards);
+console.log(hacker2Reverse);
 
+//3.3
 
 if (hacker1 < hacker2) {
-    console.log(`The driver's name goes first.`);
- } else if (hacker1 > hacker2) {
-    console.log(`Yo, the navigator goes first definitely.`);
- } else {
-    console.log(`What?! You both have the same name?`);
- };
-
- // Bonus 1
-
- let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mattis, lacus nec varius ultrices, urna nisi commodo ante, euismod facilisis tortor turpis sed lorem. Quisque commodo massa ut condimentum vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis enim sapien, mattis dignissim ligula quis, gravida ultrices mi. Curabitur faucibus vestibulum bibendum. Curabitur ante ligula, ultricies in fringilla at, facilisis id nisi. Quisque sagittis nunc nisl. Donec mollis ex vitae faucibus sollicitudin. Duis nec erat ut risus dignissim venenatis eget a tellus. Fusce aliquet velit dolor, non maximus ligula fringilla nec. In sollicitudin ante eget scelerisque consequat. Nunc suscipit sem et tempus placerat. Nam purus diam, ultricies eget luctus at, blandit non elit. Ut non efficitur neque. Duis pretium mi urna, placerat ullamcorper est vestibulum at. Donec imperdiet neque quis tincidunt maximus. Curabitur felis turpis, vulputate id ante id, varius posuere leo. Morbi eu placerat nulla, non consectetur velit. Ut non ipsum hendrerit, laoreet erat quis, gravida velit. Nullam tincidunt elit consequat sapien hendrerit faucibus. Etiam a ante luctus, varius eros a, sollicitudin libero. Nam malesuada placerat elit, in porttitor eros interdum et. Donec sit amet metus velit. Etiam luctus lobortis metus, vitae euismod elit finibus sit amet. Aliquam libero velit, molestie sed dapibus in, posuere ut erat. Praesent sed aliquet lectus, sed efficitur turpis. Praesent id sem non magna elementum congue. Nulla aliquam orci in efficitur auctor. Suspendisse potenti. Maecenas lobortis in libero a ornare.";
-
- let numberOfWords = 0;
-
- for (let i = 0; i < text.length; i++) {
-    if (text[i] === " ") {
-        numberOfWords ++
-     } else {
-        continue;
-     };
-};
-
-console.log(numberOfWords + 1);
+   console.log("The driver's name goes first.");
+} else if (hacker1 > hacker2) {
+   console.log("Yo, the navigator goes first definitely.");
+} else if (hacker1 === hacker2) {
+   console.log("What?! You both have the same name?");
+}
 
 
-let numberOfEt = 0;
+// Bonus 1
+
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam feugiat, enim id lacinia eleifend, dui mi mattis nibh, quis placerat felis nisi quis lectus. Morbi bibendum nisi a massa porttitor, et facilisis sem condimentum. Vivamus vitae facilisis eros. Donec euismod enim ex, eget tincidunt ante molestie quis. Quisque non placerat lacus, non feugiat tortor. Nullam quis commodo velit, nec pretium felis. Cras ut turpis justo. Vivamus convallis vestibulum lobortis. Sed ac aliquam purus, sed egestas leo. Sed ac tristique odio. Praesent commodo neque ac nulla efficitur, id placerat dui viverra. Nam finibus turpis mauris, vitae vulputate eros porttitor vitae. Donec eu nibh facilisis massa dictum eleifend eu et arcu. Proin vitae nulla odio. Donec auctor, quam ut fringilla semper, sapien mi viverra turpis, sed dictum ante risus ac ante. Donec eu placerat purus. Suspendisse tincidunt auctor enim, at tempor libero aliquet ut. Aliquam felis tortor, varius a sapien nec, semper tincidunt purus. Vivamus pharetra laoreet sapien, ut tincidunt mauris elementum ac. Sed mauris dui, suscipit non nibh iaculis, iaculis hendrerit nunc. Etiam quis maximus urna, in suscipit lectus. Mauris blandit, tellus vitae consectetur semper, mauris neque tincidunt ex, eu auctor nisl turpis sit amet urna. Vestibulum vitae erat in metus gravida blandit et dictum enim. Aliquam tincidunt venenatis eleifend. Phasellus porttitor, odio a ultrices hendrerit, nunc risus feugiat lorem, sit amet fermentum libero lectus quis odio. Phasellus vulputate neque sit amet libero hendrerit suscipit. Aenean faucibus varius gravida. Etiam malesuada tempus erat, a ultricies lectus imperdiet ac. Cras ullamcorper massa ac nisi sagittis laoreet. Aenean et bibendum sapien. Duis non suscipit est, in rutrum risus. Pellentesque in lectus vehicula, euismod eros quis, facilisis turpis. Donec a enim vel nunc vehicula laoreet eget id magna. Nullam sed nisi eu nisi suscipit ultricies at eu ipsum. Aliquam leo leo, imperdiet ac luctus nec, efficitur et ante. Quisque id enim id nulla sagittis iaculis in eget ex.";
+
+let wordCounter = 0
 
 for (let i = 0; i < text.length; i++) {
-    if ((text[i -1] !== "a" - "z" || text[i -1] !== "A" - "Z") && ((text[i] === "e" && text[i + 1] === "t") || (text[i] === "E" && text[i + 1] === "t")) && (text[i + 2] !== "a" - "z" || text[i + 2] !== "A" - "Z") ) {
-        numberOfEt ++
-     } else {
-        continue;
-     };
+   if (text[i] === " ") {
+      wordCounter ++;
+   }
 };
 
-console.log(numberOfEt);
+console.log(wordCounter + 1);
+
+let etCounter = 0;
+
+for (let i = 0; i < text.length; i++) {
+   if (text[i] === " " && text[i+1] === "e" && text[i+2] === "t" && text[i+3] === " " ) {
+      etCounter += 1;
+   }
+};
+
+console.log(etCounter);
+
+
+// Bonus 2
+
+const phraseToCheck = "Was it a car or a cat I saw?";
+
+const lettersString = "abcdefghijklmnopqrstuvwxyz";
+
+const clean = (string) => {
+   let phraseLower = string.toLocaleLowerCase()
+   let cleanPhrase = "";
+   for (let i = 0; i < phraseLower.length; i++) {
+      if (lettersString.includes(phraseLower[i])) {
+         cleanPhrase += phraseLower[i];
+      };
+   };
+   return cleanPhrase;
+}
+
+const isPalindrome = (string) => {
+   const cleanPhrase = clean(string);
+   for (let i = 0; i < cleanPhrase.length/2; i++) {
+      if (cleanPhrase[i] !== cleanPhrase[cleanPhrase.length - 1 - i]) {
+         return false;
+      }
+   }
+   return true;
+}
+
+console.log(isPalindrome(phraseToCheck));
